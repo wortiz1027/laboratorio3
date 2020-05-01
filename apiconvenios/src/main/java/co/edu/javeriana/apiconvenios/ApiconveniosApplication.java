2526a6be6950cfcfed306218991571c99a717bca
Wilman Ortiz <willman.ortiz@outlook.com>
2020-05-01 08:10:57 -0500

    Agrega archivos de configuracion
    Se agregan los primeros archivos de configuracion para proyecto de
    convenios como gitignore y los archivos docker

---
+++ apiconvenios/src/main/java/co/edu/javeriana/apiconvenios/ApiconveniosApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.apiconvenios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiconveniosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiconveniosApplication.class, args);
+	}
+
+}
---
+++ apiconvenios/src/test/java/co/edu/javeriana/apiconvenios/ApiconveniosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.apiconvenios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiconveniosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
