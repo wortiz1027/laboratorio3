7cacf578b6222635c6ed67ef1ae7c575d0ebee9a
Brian <bsuarezb@ucentral.edu.co>
2020-05-01 21:53:18 -0500

    Construcción Servicio Proveedores
    Se finaliza la descripción del servicio de proveedores

--- apiproveedores/src/main/java/co/edu/javeriana/apiproveedores/model/Proveedor.java
+++ apiproveedores/src/main/java/co/edu/javeriana/apiproveedores/model/Proveedor.java
@@ -1,12 +1,48 @@
 package co.edu.javeriana.apiproveedores.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import java.math.BigInteger;
+import lombok.Data;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
 @Entity
+@Table(name = "proveedor", schema = "proveedoresdb")
 public class Proveedor implements java.io.Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     @Id
-    private BigInteger idProveedor;
+    @Column(name = "ID_PROVEEDOR")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private BigDecimal idProveedor;
+
+    @NotNull
+    @Basic(optional = false)
+    @Size(min = 1, max = 100)
+    @Column(name = "NOMBRE")
+    private String nombre;
+
+    @NotNull
+    @Basic(optional = false)
+    @Size(min = 1, max = 100)
+    @Column(name = "DESCRIPCION")
+    private String descripcion;
+
+    @NotNull
+    @Column(name = "FECHA")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date fecha;
+
+    @NotNull
+    @Column(name = "ACTIVO", columnDefinition = "TINYINT(1)")
+    private Boolean esActivo;
+
+    @NotNull
+    @Column(name = "COMPENSACION", columnDefinition = "TINYINT(1)")
+    private Boolean esCompensacion;
+
 }
--- apiproveedores/src/main/java/co/edu/javeriana/apiproveedores/repositories/ProveedoresRepository.java
+++ apiproveedores/src/main/java/co/edu/javeriana/apiproveedores/repositories/ProveedoresRepository.java
@@ -1,7 +1,11 @@
 package co.edu.javeriana.apiproveedores.repositories;
 
+import co.edu.javeriana.apiproveedores.model.Proveedor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+
 @Repository
-public interface ProveedoresRepository {
+public interface ProveedoresRepository extends JpaRepository<Proveedor, BigDecimal> {
 }
